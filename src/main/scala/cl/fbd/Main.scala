adb38faf432185939fdcd215ab947f7bd0fa4795
Francois Bertrand <francois.bertrand@navigomining.com>
2016-05-27 15:19:33 -0700

    comments

--- src/main/scala/cl/fbd/Main.scala
+++ src/main/scala/cl/fbd/Main.scala
@@ -4,73 +4,60 @@ import scalanative.native._
 import scalanative.runtime.GC
 import scalanative.libc.stdlib._
 
-@link("gc")
-@extern 
-object GC2 {
-  @name("GC_get_heap_size")
-  def GC_get_heap_size(): CSize = extern
-  
-  @name("GC_gcollect")
-  def GC_gcollect () : Unit = extern
-  
-  @name("GC_get_free_bytes")
-  def GC_get_free_bytes(): CSize = extern
-}
-
-@extern
-object stdio {
-  def sprintf(s: CString, format: CString, args: Vararg*): CInt = extern
-}
-
-@extern
-object stdlib2 {
-  def atoi (s: CString): CInt = extern
-}
+/*
+ *
+ */
 
 object Main {
   def main (args : Array [String]) = {
-    fprintf(stdout, c"Test GC\n")
+    fprintf(stdout, c"Test Boehm GC\n")
     
-    // 
+    // this array will "remember" a C pointer cast as a Int value 
     val arr = new Array [Int] (2)
     
-    //
+    // should be small values
     val heapSizeBefore = GC2.GC_get_heap_size ()
-    val freeBytesBefore = GC2.GC_get_free_bytes()
+    val freeBytesBefore = GC2.GC_get_free_bytes ()
           
-    fprintf(stdout, c"Outside before useMem\n")
+    fprintf(stdout, c"main (): before useMem\n")
     
     fprintf(stdout, c"heap size: %d\n", heapSizeBefore)
     fprintf(stdout, c"free bytes: %d\n", freeBytesBefore)
+    fprintf(stdout, c"\n")
     
+    // the array allocated inside useMem should be candidate to garbage collection
+    // but arr (0) retains the pointer value as a integer
     arr (0) = useMem ()
 
-    // stop the world    
+    // The conservative GC treats the Int value as a potential pointer and
+    //   don't reclaim the memory    
     GC2.GC_gcollect ()
     
     val heapSizeAfter1 = GC2.GC_get_heap_size ()
     val freeBytesAfter1 = GC2.GC_get_free_bytes()
           
-    fprintf(stdout, c"Outside after useMem and after first gc\n")
+    fprintf(stdout, c"main (): after useMem and after first gc\n")
     
-    fprintf(stdout, c"addr as Int %d\n", arr (0))
+    fprintf(stdout, c"ptr address as Int %d\n", arr (0))
     
     fprintf(stdout, c"heap size: %d\n", heapSizeAfter1)
     fprintf(stdout, c"free bytes: %d\n", freeBytesAfter1)
-    
-    //
+    fprintf(stdout, c"\n")
+
+    // forget the pointer    
     arr (0) = 0
 
-    // stop the world    
+    // as arr don't hold the pointer value anymore, Boehm GC can claims the memory 
     GC2.GC_gcollect ()
     
-    fprintf(stdout, c"Outside after forgetting the pointer Int value and after second gc\n")
+    fprintf(stdout, c"main (): after forgetting the pointer Int value and after second gc\n")
     
     val heapSizeAfter2 = GC2.GC_get_heap_size ()
     val freeBytesAfter2 = GC2.GC_get_free_bytes()
           
     fprintf(stdout, c"heap size: %d\n", heapSizeAfter2)
-    fprintf(stdout, c"free bytes: %d\n", freeBytesAfter2)
+    fprintf(stdout, c"free bytes: %d\n", freeBytesAfter2)    
+    fprintf(stdout, c"\n")
     
     ()
   }
@@ -83,35 +70,39 @@ object Main {
   def useMem () : Int = {
     import scalanative.runtime
     
-    fprintf(stdout, c"useMem\n")
-    
     val ptr = GC.malloc (10000000).cast[Ptr[Byte]]
+    
+    fprintf(stdout, c"useMem (): before gc\n")
     
     fprintf(stdout, c"ptr address hex: %p, int: %d\n", ptr, ptr)
     
     val heapSizeBefore = GC2.GC_get_heap_size ()
     val freeBytesBefore = GC2.GC_get_free_bytes()
           
-    fprintf(stdout, c"Inside before GC\n")
-    
     fprintf(stdout, c"heap size: %d\n", heapSizeBefore)
     fprintf(stdout, c"free bytes: %d\n", freeBytesBefore)
+    fprintf(stdout, c"\n")
     
-    // stop the world    
+    
+    // the array is in used inside "useMem", so Boehm GC cannot free the memory    
     GC2.GC_gcollect ()
     
     val heapSizeAfter = GC2.GC_get_heap_size ()
     val freeBytesAfter = GC2.GC_get_free_bytes()
           
-    fprintf(stdout, c"Inside after gc\n")
+    fprintf(stdout, c"useMem (): after gc\n")
     
     fprintf(stdout, c"heap size: %d\n", heapSizeAfter)
     fprintf(stdout, c"free bytes: %d\n", freeBytesAfter)
+    fprintf(stdout, c"\n")
     
-    // do something with the memory
+    // do something with the memory (force the use of the array)
     !ptr = 'H'
     
+    // cast the pointer as integer and return the value 
     ptr2Int (ptr)
+    
+    // the array should be candidate to garbage collection
   }
   
   /*
@@ -129,8 +120,36 @@ object Main {
     // C obviously know how to treat a pointer as a integer
     stdio.sprintf (addrAsCString, c"%d", ptr)
     
-    fprintf(stdout, c"addr as C string %s\n", addrAsCString)
+    // fprintf(stdout, c"addr as C string %s\n", addrAsCString)
     
     stdlib2.atoi (addrAsCString)
   }    
+}
+
+// some external C library used in this test
+
+// gc.h
+@link("gc")
+@extern 
+object GC2 {
+  @name("GC_gcollect")
+  def GC_gcollect () : Unit = extern
+  
+  @name("GC_get_heap_size")
+  def GC_get_heap_size(): CSize = extern
+  
+  @name("GC_get_free_bytes")
+  def GC_get_free_bytes(): CSize = extern
+}
+
+// stdio.h
+@extern
+object stdio {
+  def sprintf(s: CString, format: CString, args: Vararg*): CInt = extern
+}
+
+// stdlib.h
+@extern
+object stdlib2 {
+  def atoi (s: CString): CInt = extern
 }
